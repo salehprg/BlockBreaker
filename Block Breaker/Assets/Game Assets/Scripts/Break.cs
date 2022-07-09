2e83e25af6c66cdb232a87a8a48f7bb8cc5d6fd8
Saleh <salehebrahimian@gmail.com>
2022-07-09 13:23:41 +0430

    Basic Scene

--- Block Breaker/Assets/Game Assets/Scripts/Break.cs
+++ Block Breaker/Assets/Game Assets/Scripts/Break.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class Break : MonoBehaviour
 {
     public float hp = 2.0f;
+    public Color light;
     
     int currentHit = 0;
 
     void Start()
     {
-        
+        if(hp == 5)
+        {
+            GetComponent<Renderer>().material.SetColor("Albedo" , light);
+        }
     }
 
     // Update is called once per frame
